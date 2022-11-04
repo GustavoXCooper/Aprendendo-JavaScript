da6eeb85678951587cf73e6f74f300655cb541c5
Gustavo Cooper <gustavoxcooper@gmail.com>
2022-11-04 09:26:30 -0300

    Finalizando exercício urna eletrônica

--- Exercicios de criação de página/Urna Eletrônica/script.js
+++ Exercicios de criação de página/Urna Eletrônica/script.js
@@ -10,12 +10,14 @@ let numeros = q('.d1-3');
 
 let etapaAtual = 0;
 let numero = '';
-
+let votoBranco = false;
+let votos = [];
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual]; 
     
     let numeroHtml = '';
+    numero = '';
 
     for (let i=0;i<etapa.numeros;i++){
         if(i ===0 ){      
@@ -34,7 +36,34 @@ function comecarEtapa(){
 }
 
 function atualizaInferface(){
+    let etapa = etapas[etapaAtual];
 
+    let candidato = etapa.candidatos.filter((item)=>{
+        if (item.numero === numero){
+            return true;
+        } else {
+            return false;
+        };
+    });
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+        let fotosHtml = '';
+        for (let i in candidato.fotos){
+            if (candidato.fotos[i].small === true){
+                fotosHtml += `<div class="d1-image small"><img src="images/${candidato.fotos[i].url}" alt="">Vice-Prefeito</div>`
+            } else{
+                fotosHtml += `<div class="d1-image"><img src="images/${candidato.fotos[i].url}" alt="">Vice-Prefeito</div>`
+            }
+        }
+        lateral.innerHTML = fotosHtml
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `<div class="aviso--grande pisca">VOTO NULO</div>`
+    }
 }
 
 function clicou(n) {
@@ -48,6 +77,49 @@ function clicou(n) {
             elNumero.nextElementSibling.classList.add('pisca');
         } else {
             atualizaInferface()
+        }
+    }
+}
+
+function branco(){
+    numero = ''
+    votoBranco = true;
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = `<div class="aviso--grande pisca">VOTO EM BRANCO</div>`
+    lateral.innerHTML = ''
+}
+function corrige(){
+    comecarEtapa()
+}
+function confirma(){
+
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false;
+
+    if (votoBranco == true ){
+        votoConfirmado = true
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
+
+    } else if (numero.length === etapa.numeros){
+        votoConfirmado = true
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
+    }
+
+    if (votoConfirmado == true){
+        etapaAtual++;
+        if (etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        } else {
+            q('.tela').innerHTML = `<div class="aviso--gigante pisca">FIM</div>`
+            console.log(votos)
         }
     }
 }
